9a30aa5655a5c1a2051304163db7c85cfc50c000
Devrath <devrath.dev595@gmail.com>
2021-05-18 13:18:14 +0530

    Update QuadraticTime.java

--- Notation Examples/src/com/demo/QuadraticTime.java
+++ Notation Examples/src/com/demo/QuadraticTime.java
@@ -29,6 +29,24 @@ public class QuadraticTime {
         }
 
         // Runtime Complexity = O(n^2) + O(n) => O(n^2)
+        // Remember N^2 always increases faster than N
+        // Also we need just an approximation and not exact value
+        // Here the number of print statements depends on the size of the input
+        // Also the complexity increases in quadratic time
+
+    }
+
+    public void program3(int[] numbers){
+
+        for (int i=0 ; i<numbers.length ; i++) {  // O(N)
+            for (int j=0 ; j<numbers.length ; j++) { // O(N)
+                for (int k=0 ; k<numbers.length ; k++) { // O(N)
+                    System.out.println(numbers[i]+"--"+numbers[j]+"--"+numbers[k]);
+                }
+            }
+        }
+
+        // Runtime Complexity = O(n^3)
         // Here the number of print statements depends on the size of the input
         // Also the complexity increases in quadratic time
 
