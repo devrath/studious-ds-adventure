c3f6d7809bbd4df356717b12ad444cac38d2479d
Devrath <devrath.dev595@gmail.com>
2021-05-18 11:08:08 +0530

    v

--- Notation Examples/src/com/demo/Main.java
+++ Notation Examples/src/com/demo/Main.java
@@ -3,6 +3,13 @@ package com.demo;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Start of Program");
+
+        ConstantTime object = new ConstantTime();
+
+        int[] input = {1,2,3,4,5};
+        object.program1(input);
+
+        System.out.println("Start of Program");
     }
 }
