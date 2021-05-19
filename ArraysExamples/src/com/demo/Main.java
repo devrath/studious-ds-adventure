790b469763f2a8b64f3a11d05a21656054a90dfd
Devrath <devrath.dev595@gmail.com>
2021-05-19 11:43:08 +0530

    v

--- ArraysExamples/src/com/demo/Main.java
+++ ArraysExamples/src/com/demo/Main.java
@@ -3,7 +3,7 @@ package com.demo;
 public class Main {
 
     public static void main(String[] args) {
-        DeclaringArray obj = new DeclaringArray();
-        obj.printArrayAddress();
+        FundamentalsOfArray obj = new FundamentalsOfArray();
+        obj.printArrayLength();
     }
 }
