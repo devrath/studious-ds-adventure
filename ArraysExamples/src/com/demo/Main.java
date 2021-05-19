446382cd600beb9c45f58cc53b59dd90f30cd307
Devrath <devrath.dev595@gmail.com>
2021-05-19 20:04:08 +0530

    dd

--- ArraysExamples/src/com/demo/Main.java
+++ ArraysExamples/src/com/demo/Main.java
@@ -3,13 +3,13 @@ package com.demo;
 public class Main {
 
     public static void main(String[] args) {
-        ReversalOfArray obj = new ReversalOfArray(5);
+        RemoveDuplicates obj = new RemoveDuplicates(5);
+        obj.insert(10);
         obj.insert(10);
         obj.insert(20);
+        obj.insert(20);
         obj.insert(30);
-        obj.insert(40);
-        obj.insert(50);
-        obj.reverse();
+        obj.removeDuplicates();
         obj.display();
     }
 }
