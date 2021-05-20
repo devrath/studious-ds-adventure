bf852db596a31eeabdbf8eb5f375b9fb84abd8af
Devrath <devrath.dev595@gmail.com>
2021-05-20 17:39:14 +0530

    m

--- LinkedListExamples/src/com/demo/Main.java
+++ LinkedListExamples/src/com/demo/Main.java
@@ -7,7 +7,7 @@ public class Main {
         list.addLast(10);
         list.addLast(20);
         list.addLast(30);
-        list.removeLast();
+        list.reverseList();
         System.out.println(list.getSize());
     }
 }
