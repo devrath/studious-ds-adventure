4b1f47f07607c7465dcd1816f5115353e90ddfb7
Devrath <devrath.dev595@gmail.com>
2021-05-21 09:49:10 +0530

    stacks

--- StacksExamples/src/com/demo/Main.java
+++ StacksExamples/src/com/demo/Main.java
@@ -3,8 +3,21 @@ package com.demo;
 public class Main {
 
     public static void main(String[] args) {
-        var input = ")1 + 2(";
-        var balancedExpressions = new BalancedExpressions();
-        System.out.println(balancedExpressions.isBalanced(input));
+
+        var stackUsingArray = new StackUsingArray(5);
+        stackUsingArray.push(10);
+        stackUsingArray.push(20);
+        stackUsingArray.push(30);
+        stackUsingArray.push(40);
+        stackUsingArray.push(50);
+        stackUsingArray.display();
+        stackUsingArray.pop();
+        stackUsingArray.pop();
+        stackUsingArray.pop();
+        stackUsingArray.pop();
+        stackUsingArray.pop();
+        stackUsingArray.display();
+        stackUsingArray.pop();
+
     }
 }
