87a7cdac850ff7a2e5dd7f9160469a11f8c4d469
Devrath <devrath.dev595@gmail.com>
2021-06-18 12:57:46 +0530

    moved the packages

---
+++ Java/StacksExamples/src/com/demo/Main.java
@@ -0,0 +1,10 @@
+package com.demo;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        var button = new Button();
+        button.setText("Click button")
+    }
+}
