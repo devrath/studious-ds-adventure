5fb782736f893e7b2f92df07b5d095954e4907a0
Devrath <devrath.dev595@gmail.com>
2021-07-17 10:31:55 +0530

    m

--- Application/app/src/main/java/com/example/code/ui/arrays/ArraysFragment.kt
+++ Application/app/src/main/java/com/example/code/ui/arrays/ArraysFragment.kt
@@ -1,23 +1,37 @@
 package com.example.code.ui.arrays
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.code.R
 import com.example.code.databinding.FragmentArraysBinding
 import com.example.code.ui.base.BaseFragment
 
-class ArraysFragment : BaseFragment<FragmentArraysBinding>(FragmentArraysBinding::inflate) {
+class ArraysFragment : BaseFragment<FragmentArraysBinding>(FragmentArraysBinding::inflate) , View.OnClickListener{
 
-    private lateinit var arraysViewModel: ArraysViewModel
+    private lateinit var viewModel : ArraysViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        viewModel = ViewModelProvider(this).get(ArraysViewModel::class.java)
+        binding.declareArraysId.setOnClickListener(this@ArraysFragment)
+        setObservables()
     }
+
+    private fun setObservables() {
+        viewModel.output.observe(viewLifecycleOwner, { result ->
+
+        })
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.declareArraysId -> {
+               // viewModel.declareArray()
+
+            }
+        }
+    }
+
+
 }
--- Application/app/src/main/java/com/example/code/ui/arrays/ArraysViewModel.kt
+++ Application/app/src/main/java/com/example/code/ui/arrays/ArraysViewModel.kt
@@ -5,9 +5,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ArraysViewModel : ViewModel() {
+    private val _output = MutableLiveData<String>()
+    val output: LiveData<String> = _output
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+    fun declareArray() {
+        _output.value = "Answer"
     }
-    val text: LiveData<String> = _text
 }
