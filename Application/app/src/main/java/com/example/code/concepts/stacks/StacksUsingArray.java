1075abd6f40624d9d1d0459f0cbdd9264ce2f13b
Devrath <devrath.dev595@gmail.com>
2021-07-26 21:12:58 +0530

    n

--- Application/app/src/main/java/com/example/code/concepts/stacks/StacksUsingArray.java
+++ Application/app/src/main/java/com/example/code/concepts/stacks/StacksUsingArray.java
@@ -1,14 +1,65 @@
 package com.example.code.concepts.stacks;
 
+import com.orhanobut.logger.Logger;
+
+import java.util.Arrays;
+
+/**
+ * ALGORITHM:
+ * * CREATING:--> We need to create a array of a specific size
+ * * INSERTING:-> We have a count variable of type integer kept initialized to zero
+ *  whenever we want to add a element to the array, we use the index and add the element to that index
+ *  once the element is added we increment the count value by one, thus in such a way we track the position to be added
+ *  * DELETING:-> The deleting process is simple, we just decrement the pointer giving the illusion that we removed the item but it is actually there
+ *
+**/
+
 public class StacksUsingArray {
 
-    private int sizeOfArray = 0;
+    private final int[] ourArray;
+
+    private int position = 0;
 
     public StacksUsingArray(int sizeOfArray) {
-        this.sizeOfArray = sizeOfArray;
+        ourArray = new int[sizeOfArray];
     }
 
+    /**
+     * Pushing the item to the array
+     */
+    public void pushItem(int value) {
+        if(position<=ourArray.length-1){
+            ourArray[position++] = value;
+        }else{
+            Logger.d("Array is full cannot insert item into the array");
+        }
+    }
 
+    /**
+     * Removing the item from the array
+     * @return the element removed
+     */
+    public void popItem() {
+        if(position==0) { throw new IllegalStateException(); }
+        int elementRemoved = ourArray[--position];
+        Logger.d("Element deleted is: "+elementRemoved);
+    }
+
+    /**
+     * Printing the elements of the array
+     */
+    public void printArray() {
+        String result="";
+
+        for(int i=0; i<position; i++){
+            if(i==position-1){
+                result = result+ourArray[i];
+            }else{
+                result = result+ourArray[i]+",";
+            }
+        }
+        Logger.d("Result: "+result);
+    }
 
 
 }
--- Application/app/src/main/java/com/example/code/ui/stacks/StacksViewModel.kt
+++ Application/app/src/main/java/com/example/code/ui/stacks/StacksViewModel.kt
@@ -2,23 +2,39 @@ package com.example.code.ui.stacks
 
 import androidx.lifecycle.ViewModel
 import com.example.code.concepts.stacks.BalancedExpression
+import com.example.code.concepts.stacks.StacksUsingArray
 import com.example.code.concepts.stacks.StringReversal
 import com.orhanobut.logger.Logger
+import java.util.*
 
 class StacksViewModel : ViewModel() {
 
-    fun reversingAString(input : String) {
+    fun reversingAString(input: String) {
         val result = StringReversal().initiate(input)
         Logger.d(result)
     }
 
-    fun balancedExpression(input : String) {
+    fun balancedExpression(input: String) {
         val result = BalancedExpression().isBalanced(input)
         Logger.d("Is expression balanced:-> $result")
     }
 
     fun stackUsingArray() {
+        val data = StacksUsingArray(5)
+        data.pushItem(10)
+        data.printArray()
+        data.pushItem(20)
+        data.printArray()
+        data.pushItem(30)
+        data.printArray()
+        data.pushItem(40)
+        data.printArray()
+        data.pushItem(50)
+        data.printArray()
 
+        data.popItem()
+
+        data.printArray()
     }
 
 
