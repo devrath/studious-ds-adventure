13a405bb7a54697b8a9094cdcab0ba16eabd2733
Devrath <devrath.dev595@gmail.com>
2023-11-16 11:15:42 +0530

    added code

---
+++ collection/LinkedList/concepts/README.md
@@ -0,0 +1,4 @@
+# \[ 🚧 Work in progress 👷‍♀️⛏👷🔧️👷🔧 🚧 \]
+
+# studious-ds-adventure
+This repository contains the fundamental concepts of data structures
---
+++ collection/LinkedList/ref/untitled/src/PrintElements.java
@@ -0,0 +1,55 @@
+public class PrintElements {
+
+    private Node head;
+    private Node tail;
+    private int length=0;
+
+    public PrintElements() {
+        initiate();
+        printElements();
+    }
+
+    private class Node {
+        int value;
+        Node next = null;
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private void initiate(){
+        // Create a new node
+        Node newNode = new Node(10);
+        // Point the head node to the new node
+        head = newNode;
+        // Point the tail to the new node
+        tail = newNode;
+    }
+
+
+    private void printElements(){
+        // Use the current node to track to iteration
+        Node currentNode = head;
+
+        StringBuilder buff = new StringBuilder();
+
+        buff.append("[");
+        while (currentNode!=null){
+            buff.append(head.value);
+            currentNode = currentNode.next;
+            length++;
+            if(currentNode!=null){
+                buff.append("-");
+            }
+        }
+        buff.append("]");
+
+        System.out.println("Elements ->" + buff.toString());
+        System.out.println("Head ----->" + head.value);
+        System.out.println("Tail ----->" + tail.value);
+        System.out.println("Length --->" + length);
+
+    }
+
+
+}
