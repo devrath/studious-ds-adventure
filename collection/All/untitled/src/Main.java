eaad31243dbe0b2baf1b0f5e17678a9168e52b14
Devrath <devrath.dev595@gmail.com>
2023-11-25 22:28:13 +0530

    Auto stash before merge of "main" and "origin/main"

--- collection/All/untitled/src/Main.java
+++ collection/All/untitled/src/Main.java
@@ -1,9 +1,10 @@
 import arrays.fundamentals.CustomArray;
+import arrays.programs.RemoveElement;
 import arrays.programs.ReverseArray;
 import arrays.programs.TwoSum;
 
 public class Main {
 
-    public static void main(String[] args) { new TwoSum(); }
+    public static void main(String[] args) { new RemoveElement(); }
 
 }
---
+++ collection/Arrays/Programs/RemoveElement/RemoveElement.java
@@ -0,0 +1,39 @@
+package arrays.programs;
+
+import java.util.Arrays;
+
+public class RemoveElement {
+
+    int[] inputArray = new int[]{0,1,2,2,3,0,4,2};
+    int elementToRemove = 2;
+
+    public RemoveElement(){
+
+        int leftPointer = 0;
+        int rightPointer = inputArray.length-1;
+
+        while((leftPointer<rightPointer)){
+            if(inputArray[leftPointer]!=elementToRemove){
+                // Keep incrementing left pointer
+                leftPointer++;
+            }else{
+                // Exchange left with right pointer
+                int temp = inputArray[leftPointer];
+                // Swap elements
+                inputArray[leftPointer] = inputArray[rightPointer];
+                inputArray[rightPointer] = temp;
+                // Decrement the right pointer
+                rightPointer--;
+            }
+        }
+
+        int result = leftPointer+1;
+
+        System.out.println("Result position: -->"+result);
+
+    }
+
+
+
+
+}
