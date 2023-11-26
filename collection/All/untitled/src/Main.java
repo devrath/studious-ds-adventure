3981941710bd4827aed521586bd93331e98f0f3d
Devrath <devrath.dev595@gmail.com>
2023-11-26 10:55:22 +0530

    Auto stash before merge of "main" and "origin/main"

--- collection/All/untitled/src/Main.java
+++ collection/All/untitled/src/Main.java
@@ -1,10 +1,11 @@
 import arrays.fundamentals.CustomArray;
+import arrays.programs.MergeSortedArray;
 import arrays.programs.RemoveElement;
 import arrays.programs.ReverseArray;
 import arrays.programs.TwoSum;
 
 public class Main {
 
-    public static void main(String[] args) { new RemoveElement(); }
+    public static void main(String[] args) { new MergeSortedArray(); }
 
 }
---
+++ collection/Arrays/Programs/MergeSortedArray/MergeSortedArray.java
@@ -0,0 +1,45 @@
+package arrays.programs;
+
+import java.util.Arrays;
+
+public class MergeSortedArray {
+
+
+
+    public MergeSortedArray(){
+        int[]  nums1 = new int[]{0};
+        int m = 0;
+        int[] nums2 = new int[]{1};
+        int n = 1;
+
+        merge(nums1,m,nums2,n);
+
+        System.out.println(Arrays.toString(Arrays.stream(nums1).toArray()));
+    }
+
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int resultEndPointer = nums1.length-1;
+        int leftDataSetPointer = m-1;
+        int rightDataSetPointer = n-1;
+
+        // Since we need to put all elements from right array to left array --> We take the right pointer
+        while(rightDataSetPointer>=0){
+            // If data at left pointer is greater than right pointer
+            // We mention left pointer condition because it can be -1 at a point
+            if(leftDataSetPointer>=0 && nums1[leftDataSetPointer] > nums2[rightDataSetPointer]){
+                // Copy the left pointer position to result pointer at left
+                nums1[resultEndPointer]=nums1[leftDataSetPointer];
+                leftDataSetPointer--;
+                resultEndPointer--;
+            }else{
+                // Copy the right pointer position to result pointer at left
+                nums1[resultEndPointer]=nums2[rightDataSetPointer];
+                rightDataSetPointer--;
+                resultEndPointer--;
+            }
+        }
+    }
+
+}
