a306f7366cc55d9248b325931649acef62a19d79
Devrath <devrath.dev595@gmail.com>
2023-11-29 12:26:05 +0530

    Auto stash before merge of "main" and "origin/main"

--- collection/All/untitled/src/Main.java
+++ collection/All/untitled/src/Main.java
@@ -1,8 +1,22 @@
+import BinaryTrees.CustomTree;
 import arrays.fundamentals.CustomArray;
 import arrays.programs.*;
 
 public class Main {
 
-    public static void main(String[] args) { new SingleNumber(); }
+    public static void main(String[] args) {
+
+        CustomTree tree = new CustomTree();
+        tree.insert(100);
+        tree.insert(75);
+        tree.insert(150);
+        //tree.insert(25);
+        tree.insert(85);
+        tree.insert(125);
+        tree.insert(175);
+        tree.countLeaves();
+
+
+    }
 
 }
