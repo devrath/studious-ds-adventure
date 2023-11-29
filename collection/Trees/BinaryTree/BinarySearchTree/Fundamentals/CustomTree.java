9b5860abd1f8b5c7ff8ca66b2f0a89875ab2768d
Devrath <devrath.dev595@gmail.com>
2023-11-29 13:36:53 +0530

    added

---
+++ collection/Trees/BinaryTree/BinarySearchTree/Fundamentals/CustomTree.java
@@ -0,0 +1,213 @@
+package BinaryTrees;
+
+
+/**
+ * SUPPORTED OPERATIONS:
+ * ---------------------
+ * <1> INSERTION    :-> Inserting a new value into the binary tree </1>
+ * <2> FIND-VALUE   :-> Finding if a value is present in the binary tree(Iteration) </2>
+ * <3> MAX-VALUE    :-> Finding the maximum value of the binary tree </3>
+ * <4> TREE-SIZE    :-> Finding the size of the binary tree </4>
+ * <5> COUNT-LEAVES :-> Counting the number of leaves in tree </5>
+ * <6> Ancestor     :-> Implement a method to return the ancestors of a value in a List<Integer></6>
+ */
+
+public class CustomTree {
+
+    private Node root = null;
+
+
+     /**
+     * ************************************* TREE-OPERATIONS *************************************
+     **/
+
+    /**
+     * Inserting a Node
+     */
+    public void insert(int value) {
+
+        Node current = root;
+
+        if(current==null){
+            // There are no elements inserted, so we perform the first insertion operation
+            root = createNode(value);
+        }else{
+            Node nodeToInsert = createNode(value);
+            // Iterate to the leaf node from the root where we need to perform insertion
+            while (current!=null){
+                // Either we insert to left of root or we insert to right of root
+                if(value < current.value){
+                    if(current.right==null){
+                        current.left = nodeToInsert;
+                        break;
+                    }else{
+                        current = current.left;
+                    }
+                }else{
+                    if(current.right==null){
+                        current.right = nodeToInsert;
+                        break;
+                    }else{
+                        current = current.right;
+                    }
+                }
+            }
+        }
+
+    }
+
+    /**
+     * Finding a node based on the value
+     */
+    public boolean find(int value){
+        Node current = root;
+        boolean isValueFound = false;
+
+        while(current!=null){
+            if(value==current.value){
+                isValueFound = true;
+                break;
+            }else{
+                if(value<current.value){
+                    // Traverse to left sub-tree since value might be on left
+                    current = current.left;
+                }else{
+                    // Traverse to right of sub-tree since value might be on right
+                    current = current.right;
+                }
+            }
+        }
+        return isValueFound;
+    }
+
+    /**
+     * Finding the maximum value in the binary tree
+     */
+    public void findMaxValue(){
+        Node current = root;
+
+        if(current==null){
+            // Empty tree
+            System.out.println("No elements are present");
+        }else{
+            int maxElement = 0;
+            // Elements are present in the tree, So there is possibility of element existing.
+            if(current.right==null){
+                // There is just one element, So that element itself will the max element
+                maxElement = current.value;
+            }else{
+                while(current!=null){
+                    maxElement = current.value;
+                    current=current.right;
+                }
+            }
+            System.out.println("Maximum element is:-> "+maxElement);
+        }
+    }
+
+    /**
+     * Finding the number of elements in tree
+     */
+    public void findTreeSize(){
+        Node current = root;
+        int treeSize = 0;
+        if(current!=null){
+            // At-lest one element is present
+            if((current.left==null) && (current.right==null)){
+                // Only one element is present
+                treeSize = 1;
+            }else{
+                // More than one element can be present
+                // Explanation:-> We increment (+1) here at end because root node is not taken in recursive function, So we explicitly add it
+                treeSize = recursiveFindSize(current,treeSize)+1;
+            }
+        }
+        System.out.println("Tree Size: ->"+treeSize);
+    }
+
+    /**
+     * Finding the number of leaves in the tree
+     */
+    public void countLeaves(){
+        Node current = root;
+        int noOfLeaves = 0;
+        if(current!=null){
+            if((current.left==null) && (current.right==null)){
+                // Just one element is present, It itself is the leaf node
+                noOfLeaves=1;
+            }else{
+                // More than one element is present, So there is a possibility of more than one leaf nodes
+                noOfLeaves = recursiveFindLeavesSize(current,noOfLeaves);
+            }
+        }
+        System.out.println("Number of leaves in tree: ->"+noOfLeaves);
+    }
+
+
+
+    /**
+     * ************************************* TREE-OPERATIONS *************************************
+     **/
+
+
+    private int recursiveFindLeavesSize(Node current, int noOfLeaves) {
+        // Check if it is a leaf node (Both left and right subtree must be null)
+        if((current.left==null) && (current.right==null)){
+            // This is leaf node
+            noOfLeaves++;
+        }else{
+            if(current.left!=null){
+                noOfLeaves = recursiveFindSize(current.left,noOfLeaves);
+            }
+            if(current.right!=null){
+                noOfLeaves = recursiveFindSize(current.right,noOfLeaves);
+            }
+        }
+        return noOfLeaves;
+    }
+
+    private int recursiveFindSize(Node current, int treeSize) {
+        if(current.left!=null){
+            treeSize = recursiveFindSize(current.left,++treeSize);
+        }
+        if(current.right!=null){
+            treeSize = recursiveFindSize(current.right,++treeSize);
+        }
+        return treeSize;
+    }
+
+
+    public Node printResult(){
+        return root;
+    }
+
+    /**
+     * Iterate to leaf node
+     **/
+    private Node iterateToLeafNode(int value, Node current) {
+        // Iterate until we reach the leaf node
+        return current;
+    }
+
+    /**
+     * Creating a new Node
+     */
+    private Node createNode(int value) {
+        return new Node(value);
+    }
+
+    private class Node {
+        Node left;
+        Node right;
+        int value;
+        public  Node(int value){
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Node=" + value;
+        }
+    }
+
+}
---
+++ collection/Trees/BinaryTree/BinarySearchTree/README.md
@@ -0,0 +1,69 @@
+<div align="center">
+  
+| `Contents` |
+| ---------- |
+| [What is binary tree](https://github.com/devrath/studious-ds-adventure/blob/main/collection/Trees/BinaryTree/README.md#what-is-a-binary-tree) |
+| [What is binary search tree(BST)](https://github.com/devrath/studious-ds-adventure/blob/main/collection/Trees/BinaryTree/README.md#what-is-binary-search-treebst) |
+| [What is the difference between a normal tree and a binary tree](https://github.com/devrath/studious-ds-adventure/blob/main/collection/Trees/BinaryTree/README.md#what-is-binary-search-treebst) |
+| [What is the difference between a binary tree and a binary-search-tree](https://github.com/devrath/studious-ds-adventure/blob/main/collection/Trees/BinaryTree/README.md#what-is-the-difference-between-a-binary-tree-and-a-binary-search-tree) |
+| [Time complexity](https://github.com/devrath/studious-ds-adventure/blob/main/collection/Trees/BinaryTree/README.md#%EF%B8%8F-time-complexity) |
+
+</div>
+
+
+## `What is a binary tree`
+
+<div align="center">
+  
+![bst](https://github.com/devrath/studious-ds-adventure/assets/1456191/cb132f95-0031-4a49-9bea-0b1d9bf6a50b)
+
+</div>
+
+* A tree is called a binary tree if the data is organized in a hierarchical structure and a `node` has two children namely `left` and `right`.
+* There can be max of two children for a node and a node can have one child or no child also.
+* The elements to the left of the node are called the `left-subtree` of that node and the elements to the right of the node are called the `right-subtree` of that node.
+
+## `What is binary search tree(BST)`
+
+<div align="center">
+  
+![bst-1](https://github.com/devrath/studious-ds-adventure/assets/1456191/2aeab9ec-b11d-43f7-8ea6-c0ee55834d80)
+
+</div>
+
+* A tree is called a binary search tree if it is fundamentally a binary tree meaning it has 2 children only.
+* All the elements in the left sub-tree are lesser than the node under consideration.
+* Subsequently all the elements in the right sub-tree are greater than the node under consideration.
+* The left and right sub-tree must also be a binary search tree.
+* The entire tree must not contain any duplicate nodes.
+
+
+## `What is the difference between a normal tree and a binary tree`
+
+
+| `Normal tree` | `Binary Treee` |
+| ------------- | --------------------- |
+| ![bst-4](https://github.com/devrath/studious-ds-adventure/assets/1456191/003192ea-94c4-4c62-8725-c23116075727) | ![bst](https://github.com/devrath/studious-ds-adventure/assets/1456191/cb132f95-0031-4a49-9bea-0b1d9bf6a50b) | 
+
+In a `normal tree`, A node can have `n-children` but in a `binary tree` a node can have a maximum of two children, also no children, just one child.
+
+
+## `What is the difference between a binary tree and a binary-search-tree`
+
+| `Binary tree` | `Binary Search Treee` |
+| ------------- | --------------------- |
+| ![bst](https://github.com/devrath/studious-ds-adventure/assets/1456191/cb132f95-0031-4a49-9bea-0b1d9bf6a50b) | ![bst-1](https://github.com/devrath/studious-ds-adventure/assets/1456191/2aeab9ec-b11d-43f7-8ea6-c0ee55834d80) | 
+
+* `Duplicates`
+  *  Binary Tree -> Contains duplicates.
+  *  Binary search tree -> Does not contain duplicates.
+* `Time complexity`
+  * Binary Tree -> Its slower around order `O(n)`
+  * Binary search tree -> It is faster since its ordered and of `O(nlogn)`
+ 
+## ⏲️ `Time complexity`  
+| `Operation` | `Complexity` |
+| ----------- | ------------ |
+| Insert | O(logn) |
+| Deletion | O(logn) |
+| Lookup | O(logn) |
