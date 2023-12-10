bace4a7bd4151df9398b2343ce0e1823e75c2b3b
Devrath <devrath.dev595@gmail.com>
2023-12-10 16:57:29 +0530

    added

---
+++ collection/Trees/BinaryTree/BinarySearchTree/Programs/EqualityChecking/EqualityChecking.java
@@ -0,0 +1,83 @@
+package BinaryTrees;
+
+public class MinimumValueInTree {
+
+    private Node root = null;
+
+    public void calculateMinimumValue() {
+        Node current = root;
+        int result = min(root);
+        System.out.println("Result:->: "+result);
+    }
+
+    private int min(Node root) {
+
+        if(root.left==null && root.right==null){
+            // Breaking condition
+            return root.value;
+        }
+
+        // Keep iterating to left
+        return min(root.left);
+    }
+
+
+    /** ************************ HELPER FUNCTIONS ********************** **/
+    /**
+     * Inserting a Node
+     */
+    public void insert(int value) {
+
+        Node current = root;
+
+        if(current==null){
+            // There are no elements inserted, so we perform the first insertion operation
+            root = createNode(value);
+        }else{
+            Node nodeToInsert = createNode(value);
+            // Iterate to the leaf node from the root where we need to perform insertion
+            while (current!=null){
+                // Either we insert to left of root or we insert to right of root
+                if(value < current.value){
+                    if(current.right==null){
+                        current.left = nodeToInsert;
+                        break;
+                    }else{
+                        current = current.left;
+                    }
+                }else{
+                    if(current.right==null){
+                        current.right = nodeToInsert;
+                        break;
+                    }else{
+                        current = current.right;
+                    }
+                }
+            }
+        }
+
+    }
+
+    public Node printResult(){
+        return root;
+    }
+
+    private Node createNode(int value) {
+        return new Node(value);
+    }
+
+    private class Node {
+        Node left;
+        Node right;
+        int value;
+        public  Node(int value){
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Node=" + value;
+        }
+    }
+    /** ************************ HELPER FUNCTIONS ********************** **/
+}
