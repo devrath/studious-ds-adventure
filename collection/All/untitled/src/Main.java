825020ea318dd0880798dd284b724e731de630a5
Devrath <devrath.dev595@gmail.com>
2023-12-13 15:50:23 +0530

    Auto stash before merge of "main" and "origin/main"

--- collection/All/untitled/src/Main.java
+++ collection/All/untitled/src/Main.java
@@ -1,22 +1,12 @@
-import BinaryTrees.*;
-import arrays.fundamentals.CustomArray;
-import arrays.programs.*;
-import recursion.FactorialOfNumber;
+
+import sortingAlgorithms.BubbleSort;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        NodesAtKDistance node = new NodesAtKDistance();
-        node.insert(7);
-        node.insert(4);
-        node.insert(9);
-        node.insert(1);
-        node.insert(6);
-        node.insert(8);
-        node.insert(10);
+        new BubbleSort();
 
-        node.printNodes(2);
 
     }
 
