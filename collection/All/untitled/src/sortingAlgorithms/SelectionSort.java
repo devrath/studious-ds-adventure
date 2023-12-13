05634d0d6cee93cc67f8cc5b2ca406fd2a309823
Devrath <devrath.dev595@gmail.com>
2023-12-13 16:22:13 +0530

    Auto stash before merge of "main" and "origin/main"

---
+++ collection/All/untitled/src/sortingAlgorithms/SelectionSort.java
@@ -0,0 +1,32 @@
+package sortingAlgorithms;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    int[] input = new int[]{8,2,4,1,3};
+
+    public SelectionSort() { performSorting(); }
+
+    private void performSorting() {
+
+        int size = input.length;
+
+        System.out.println("BEFORE SORTING:-> "+Arrays.toString(input));
+
+        for(int i=0; i<size ; i++){
+            for(int j=1 ; j<size-i ; j++){
+                if(input[j-1]>input[j]){
+                    int temp = input[j-1];
+                    input[j-1] = input[j];
+                    input[j] = temp;
+                }
+            }
+        }
+
+        System.out.println("AFTER SORTING:-> "+Arrays.toString(input));
+
+    }
+
+
+}
