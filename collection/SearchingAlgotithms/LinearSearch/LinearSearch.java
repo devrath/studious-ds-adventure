4c588e35a1b4697a59c773bfb65a94340fc62391
Devrath <devrath.dev595@gmail.com>
2023-12-19 11:38:28 +0530

    new changes

---
+++ collection/SearchingAlgotithms/LinearSearch/LinearSearch.java
@@ -0,0 +1,23 @@
+package searchingAlgorithms;
+
+import java.util.Arrays;
+
+public class LinearSearch {
+
+    int[] input = new int[]{8,2,4,1,3};
+
+    public LinearSearch() { performSearch(); }
+
+    private void performSearch() {
+
+        System.out.println("BEFORE SEARCHING:-> "+Arrays.toString(input));
+
+
+
+
+        System.out.println("AFTER SORTING:-> "+ Arrays.toString(input));
+
+    }
+
+
+}
---
+++ collection/SearchingAlgotithms/README.md
@@ -0,0 +1,11 @@
+<h1 align="center">Sorting Algorithms</h1>
+
+<div align="center">
+
+| **`Programs`** |
+| ---------- |
+| [Bubble Sort](https://github.com/devrath/studious-ds-adventure/tree/main/collection/SortingAlgorithms/BubbleSort) |
+| [Selection Sort](https://github.com/devrath/studious-ds-adventure/tree/main/collection/SortingAlgorithms/SelectionSort) |
+
+</div>
+
