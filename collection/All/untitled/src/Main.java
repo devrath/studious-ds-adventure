0aec7cf609c0762aed98ff414913d85245a3c818
Devrath <devrath.dev595@gmail.com>
2023-12-20 14:17:25 +0530

    Added

--- collection/All/untitled/src/Main.java
+++ collection/All/untitled/src/Main.java
@@ -1,11 +1,12 @@
 
+import arrays.programs.medium.LongestSubStringWithoutRepeatingCharacters;
 import strings.Palindrome;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        new Palindrome();
+        new LongestSubStringWithoutRepeatingCharacters();
 
 
     }
--- collection/All/untitled/src/arrays/programs/medium/LongestSubStringWithoutRepeatingCharacters.java
+++ collection/All/untitled/src/arrays/programs/medium/LongestSubStringWithoutRepeatingCharacters.java
@@ -1,19 +1,43 @@
 package arrays.programs.medium;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestSubStringWithoutRepeatingCharacters {
 
-    int[] inputArray = new int[]{3,3};
-    int target = 6;
-
+    String input = "pwwkew";
 
     public LongestSubStringWithoutRepeatingCharacters(){
 
+        Map<Character,Integer> visitedCharacters = new HashMap<>();
 
+        int left = 0;
+        int size = input.length();
+        int max = 0;
 
+        // Keep looping until the right pointer reaches to the end of the loop
+        for (int right = 0; right < size; right++) {
+            // Current character
+            char currentCharacter = input.charAt(right);
 
+            if(
+                    // If we have visited the characters before
+                    visitedCharacters.containsKey(currentCharacter) &&
+                    // If the current character position is also part of substring window we are currently at
+                    visitedCharacters.get(currentCharacter)>=left
+            ){
+                // Move the left pointer one past where that repeating character is
+                left = visitedCharacters.get(currentCharacter)+1;
+            }
 
-        //System.out.println(Arrays.toString(Arrays.stream(answer).toArray()));
+            // We added plus one because both pointers may point to same character
+            max = Math.max(max,right-left+1);
+            // Regardless add the next character indicating / updating the position of character
+            visitedCharacters.put(currentCharacter,right);
+        }
+
+        System.out.println("Result:-> "+max);
 
     }
 
